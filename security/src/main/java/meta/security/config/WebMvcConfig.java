d1a8094b4816b1bdf491294bc176d8ead1d5c297
ohjinhokor <ohjinho7@gmail.com>
2022-12-05 01:11:52 +0900

    시큐리티 2강 - 시큐리티 설정

--- security/src/main/java/meta/security/config/WebMvcConfig.java
+++ security/src/main/java/meta/security/config/WebMvcConfig.java
@@ -12,8 +12,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	public void configureViewResolvers(ViewResolverRegistry registry) {
 		MustacheViewResolver resolver = new MustacheViewResolver();
 		resolver.setCharset("UTF-8");
-		resolver.setContentType("text/html; charset=UTF-8");
+		resolver.setContentType("text/html;charset=UTF-8");
 		resolver.setPrefix("classpath:/templates/");
 		resolver.setSuffix(".html");
+
+		registry.viewResolver(resolver);
 	}
 }
--- security/src/main/java/meta/security/controller/IndexController.java
+++ security/src/main/java/meta/security/controller/IndexController.java
@@ -2,12 +2,46 @@ package meta.security.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping
 public class IndexController {
 
-	@GetMapping
+	@GetMapping("/index")
 	public String index() {
 		return "index";
+	}
+
+	@GetMapping("user")
+	public String user() {
+		return "user";
+	}
+
+	@GetMapping("admin")
+	public String admin() {
+		return "admin";
+	}
+
+	@GetMapping("manager")
+	public String manager() {
+		return "manager";
+	}
+
+	@GetMapping("login")
+	public String login() {
+		return "login";
+	}
+
+	@GetMapping("join")
+	public String join() {
+		return "join";
+	}
+
+	@GetMapping("joinProc")
+	@ResponseBody
+	public String joinProc() {
+		return "join Success";
 	}
 }
